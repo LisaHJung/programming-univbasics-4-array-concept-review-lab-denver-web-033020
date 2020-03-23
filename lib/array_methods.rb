105850efa1dc03a8039cf4e607e278d8b4ece868
Lisa Jung <lisahjung@gmail.com>
2020-03-23 16:54:05 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,40 @@
 def find_element_index(array, value_to_find)
-array
+  if array.include? value_to_find
+    array.index(value_to_find)
+  else
+    nil
+  end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 =begin
 
 def find_element_index(array, value_to_find)
